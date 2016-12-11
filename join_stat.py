418b62d09cd3cbc43b9fedfaed4fe1b830a51168
benjamin <benjaminelkrieff@ymail.com>
2016-12-12 00:29:25 +0100

    modification de l'algo permettant d'obtenir les matrices X et Y pour le modèle

--- join_stat.py
+++ join_stat.py
@@ -1,16 +1,15 @@
 # coding: utf-8
 import csv
 
-fichiers = ['2007:2008_Stats.csv','2008:2009_Stats.csv','2009:2010_Stats.csv','2010:2011_Stats.csv','2011:2012_Stats.csv','2012:2013_Stats.csv','2013:2014_Stats.csv','2014:2015_Stats.csv','2015:2016_Stats.csv','2016:2017_Stats.csv']
-
-Xfile = open('X.csv','w')
-Yfile = open('Y.csv','w')
+fichiers = ['2007:2008','2008:2009','2009:2010','2010:2011','2011:2012','2012:2013','2013:2014','2014:2015','2015:2016','2016:2017']
+Xfile = open('X_final.csv','w')
+Yfile = open('Y_final.csv','w')
 
 Xwriter = csv.writer(Xfile,delimiter=',',dialect='excel',lineterminator='\n')
 Ywriter = csv.writer(Yfile,delimiter=',',dialect='excel',lineterminator='\n')
 
 for fic in fichiers :
-    seasonFile = open('saisonsStatsCSV/'+fic+'','r')
+    seasonFile = open('SaisonsStatsFinales/'+fic+'_Stats_final.csv','r')
     reader = csv.reader(seasonFile,delimiter=",")
     for row in reader :
         Xwriter.writerow(row[1:len(row)-1])
